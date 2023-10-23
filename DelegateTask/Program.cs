5f044ddfa98972d53c8661a79a7eeaf64424b5ef
Adil <99889589+Ad1l9@users.noreply.github.com>
2023-10-24 00:00:00 +0400

    Add project files.

---
+++ DelegateTask/Program.cs
@@ -0,0 +1,36 @@
+﻿using DelegateTask.Models;
+
+namespace DelegateTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1
+
+            List<Person> people = new List<Person>()
+           {
+               new Person("Adil","Nasirli",18),
+               new Person("Kamil","Nasirli",19),
+               new Person("Adil","Kamilov",25),
+               new Person("Suleyman","Aslanli",15),
+               new Person("Leyla","Shafi",18),
+           };
+
+            foreach (Person person in people.FindAll(p => p.Name == "Kamil"))
+            {
+                Console.WriteLine(person);
+            }
+
+            foreach (Person person in people.FindAll(p => p.Surname.EndsWith("ova") || p.Surname.EndsWith("ov")))
+            {
+                Console.WriteLine(person);
+            }
+
+            foreach (Person person in people.FindAll(p => p.Age >= 20))
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
